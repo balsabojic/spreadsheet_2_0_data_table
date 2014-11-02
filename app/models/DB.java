5f36db7ace89fbf1b1be7fef6aecb3bbdfb74b55
Balsa Bojic <balsha.bojic@gmail.com>
2014-11-02 15:32:06 +0100

    I have changed this class a little bit. My idea was to make this class as a Singleton (Pattern). That means that we will always have only one instance of the class DB. We will always make our database one time, and then use it during the life of the app. Also I think tha t it is good to use patterns always when it is possible in order to have a good software architecture. Therefore we don't need database initialization on the beginning of the app (in the Global.java). Initialization will be done when the instance of our database is first time called.

--- app/models/DB.java
+++ app/models/DB.java
@@ -1,40 +1,76 @@
 package models;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import play.Play;
 
 import java.net.UnknownHostException;
+import java.util.Arrays;
 
 public class DB {
-    protected static MongoClient _mongoClient;
-    protected static com.mongodb.DB _db;
+    private static DB instance = null;
 
-    public static MongoClient mongoClient() {
-        if (_mongoClient == null) {
-            String connUri = Play.application().configuration().getString("mongodb.host");
+    private static final String connUri = Play.application().configuration().getString("mongodb.host");
+    private static final String defaultDB = Play.application().configuration().getString("mongodb.default");
+
+    private MongoClient mongoClient;
+    private com.mongodb.DB mongoDB;
+
+    private DB() {
+        if (mongoClient == null) {
             MongoClientURI uri = new MongoClientURI(connUri);
             try {
-                _mongoClient = new MongoClient(uri);
+                mongoClient = new MongoClient(uri);
             } catch (UnknownHostException e) {
                 e.printStackTrace();
             }
         }
-
-        return _mongoClient;
-    }
-
-    public static com.mongodb.DB db() {
-        if (_db == null) {
-            String defaultDB = Play.application().configuration().getString("mongodb.default");
-            _db = mongoClient().getDB(defaultDB);
+        if (mongoDB == null) {
+            mongoDB = mongoClient.getDB(defaultDB);
         }
-
-        return _db;
+        if (!mongoDB.collectionExists("Type")) {
+            initializeDB();
+        }
     }
 
-    public static DBCollection collection(String name) {
-        return db().getCollection(name);
+    public static DB getInstance() {
+        if (instance == null) {
+            instance = new DB();
+        }
+        return instance;
+    }
+
+    /**
+     * Initialize fake data in mongodb.
+     * This function will be called whenever the app sees now collections in
+     * the default mongodb database. When this function changed, please manually
+     * drop your mongodb so the data will be re-generated again.
+     *
+     * To drop a database in mongodb, type following command in mongodb:
+     * > use spreadsheet
+     * > db.dropDatabase()
+     */
+    private void initializeDB() {
+        System.out.println("Initialize test data");
+        DBCollection types = mongoDB.getCollection("Type");
+        BasicDBObject courseType = new BasicDBObject("name", "Course");
+        BasicDBObject personType = new BasicDBObject("name", "Person");
+        types.insert(courseType);
+        types.insert(personType);
+
+        DBCollection instances = mongoDB.getCollection("Instance");
+        instances.insert(new BasicDBObject("type", courseType.get("_id"))
+                .append("attributes",
+                        Arrays.asList(
+                                new BasicDBObject("title", "Practical Course Software Engineering for Business Information Systems"),
+                                new BasicDBObject("content", "In this course, students realize in small teams an innovative web application of medium complexity in Java."))
+                ));
+        instances.insert(new BasicDBObject("type", courseType.get("_id")));
+    }
+
+    protected com.mongodb.DB getMongoDB() {
+        return mongoDB;
     }
 }
