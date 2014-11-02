6b1c8c8a4354513143ce32def6e9b4c8dcec50fd
Balsa Bojic <balsha.bojic@gmail.com>
2014-11-02 15:35:54 +0100

    Here I have only migrated all the code in the Collection class, so we have now a nice structure, all we need is to create a collection and get the JSON for rendering.

--- app/controllers/Types.java
+++ app/controllers/Types.java
@@ -3,18 +3,17 @@ package controllers;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.mongodb.DBCursor;
+import models.Collection;
+import models.CollectionType;
 import models.DB;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Types extends Controller {
+
     public static Result index() {
-        DBCursor cursor = DB.collection("Type").find();
-        ArrayNode result = new ArrayNode(JsonNodeFactory.instance);
-        while (cursor.hasNext()) {
-            result.add(Json.parse(cursor.next().toString()));
-        }
-        return ok(result);
+        Collection collectionType = new CollectionType();
+        return ok(collectionType.getJSON().toString());
     }
 }
