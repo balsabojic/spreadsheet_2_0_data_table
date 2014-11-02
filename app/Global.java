51294ab48add6d404a0e2afddd76fd9e10c82194
Balsa Bojic <balsha.bojic@gmail.com>
2014-11-02 15:34:39 +0100

    As I have created a Singleton structure for our database, this class will not be needed anymore. But so far I think that we can use it in the future if we want to try to increase performance of the app.

--- app/Global.java
+++ app/Global.java
@@ -9,40 +9,7 @@ import java.util.Arrays;
 public class Global extends GlobalSettings {
     @Override
     public void onStart(Application app) {
-        // To re-initialize the fake data you have to drop the mongodb database
-        // In mongo shell type the following command:
-        // > use spreadsheet
-        // > db.dropDatabase()
-        if (!DB.db().collectionExists("Type")) {
-            initializeDB();
-        }
-    }
-
-    /**
-     * Initialize fake data in mongodb.
-     * This function will be called whenever the app sees now collections in
-     * the default mongodb database. When this function changed, please manually
-     * drop your mongodb so the data will be re-generated again.
-     *
-     * To drop a database in mongodb, type following command in mongodb:
-     * > use spreadsheet
-     * > db.dropDatabase()
-     */
-    private void initializeDB() {
-        System.out.println("Initialize test data");
-        DBCollection types = DB.collection("Type");
-        BasicDBObject courseType = new BasicDBObject("name", "Course");
-        BasicDBObject personType = new BasicDBObject("name", "Person");
-        types.insert(courseType);
-        types.insert(personType);
-
-        DBCollection instances = DB.collection("Instance");
-        instances.insert(new BasicDBObject("type", courseType.get("_id"))
-                .append("attributes",
-                        Arrays.asList(
-                                new BasicDBObject("title", "Practical Course Software Engineering for Business Information Systems"),
-                                new BasicDBObject("content", "In this course, students realize in small teams an innovative web application of medium complexity in Java."))
-                ));
-        instances.insert(new BasicDBObject("type", courseType.get("_id")));
+        // So far I think that this function is not needed
+        // but it can be of use, if we want to use this class to increase the performance. For example if we want to load all the data before the app is started
     }
 }
