91dce3b70bd124950c0788df4aebae40dac4bc8f
Saroj Gautam <sarojnp2003@gmail.com>
2014-11-14 23:44:59 +0100

    code clean up. Removed unused imports.

--- app/controllers/Types.java
+++ app/controllers/Types.java
@@ -1,12 +1,7 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.mongodb.DBCursor;
 import models.Collection;
 import models.CollectionType;
-import models.DB;
-import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
