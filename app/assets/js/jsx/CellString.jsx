6c406764c7f987c9f89ef1575f50228f3701014a
Duc Nguyen <duc.ngtuan@gmail.com>
2014-12-07 13:42:37 +0100

    editing state for string input cell (not finished, need code for sending request to backend)

--- app/assets/js/jsx/CellString.jsx
+++ app/assets/js/jsx/CellString.jsx
@@ -1,8 +1,34 @@
 angular.module('Spreadsheet.jsx')
   .factory('CellString', function () {
     return React.createClass({
+      getInitialState: function () {
+        return {isEditing: false};
+      },
+      onClick: function () {
+        this.setState({isEditing: true}, function () {
+          this.refs.input.getDOMNode().focus();
+        });
+      },
+      onKeyPress: function (e) {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          this.handleUpdate();
+        }
+      },
+      handleUpdate: function () {
+        console.log(this.refs.input.getDOMNode().value);
+        this.setState({isEditing: false});
+      },
       render: function () {
-        return (<td>{this.props.value}</td>);
+        if (this.state.isEditing) {
+          return (
+            <td>
+              <input type="text" ref="input" defaultValue={this.props.value}
+              onBlur={this.handleUpdate} onKeyPress={this.onKeyPress} />
+            </td>
+            );
+        }
+        return (<td onClick={this.onClick}>{this.props.value}</td>);
       }
     });
   });
