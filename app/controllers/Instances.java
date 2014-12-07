cc62853688f3ba2789d680a1372fe2e891e39e0d
Balsa Bojic <balsha.bojic@gmail.com>
2014-12-07 18:10:25 +0100

    Routes for adding and updating rows

--- app/controllers/Instances.java
+++ app/controllers/Instances.java
@@ -1,18 +1,46 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import models.CollectionInstance;
 import play.mvc.Controller;
+import play.mvc.Result;
 
 public class Instances extends Controller{
 
-    public static void updateInstanceAttribute(String id, String name, String value) {
-        CollectionInstance instance = new CollectionInstance();
-        instance.updateInstanceAttribute(id, name, value);
+    public static Result addInstace() {
+        JsonNode json = request().body().asJson();
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String type = json.findPath("type_id").asText();
+            String name = json.findPath("attribute_name").asText();
+            String value = json.findPath("attribute_value").asText();
+            if(type == null || name == null || value == null) {
+                return badRequest("Missing one of the parameters");
+            } else {
+                CollectionInstance instance = new CollectionInstance();
+                instance.addNewInstance(type, name, value);
+                return ok("Success " + type + " " + name + " " + value);
+            }
+        }
     }
 
-    public static void addNewInstance(String typeId, String attributeName, String attributeValue) {
-        CollectionInstance instance = new CollectionInstance();
-        instance.addNewInstance(typeId, attributeName, attributeValue);
+    public static Result updateInstance() {
+        JsonNode json = request().body().asJson();
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            String id = json.findPath("instance_id").asText();
+            String name = json.findPath("attribute_name").asText();
+            String value = json.findPath("attribute_value").asText();
+            if(id == null || name == null || value == null) {
+                return badRequest("Missing one of the parameters");
+            } else {
+                CollectionInstance instance = new CollectionInstance();
+                instance.updateInstanceAttribute(id, name, value);
+                return ok("Success " + id + " " + name + " " + value);
+            }
+        }
     }
 
     public static void deleteInstance(String instance_id) {
