4e64296ee0247764ecbc592914587247c5c29615
Saroj Gautam <sarojnp2003@gmail.com>
2014-12-15 14:11:06 +0100

    adding number type for numbers

---
+++ app/assets/js/jsx/CellNumber.jsx
@@ -0,0 +1,43 @@
+angular.module('Spreadsheet.jsx')
+  .factory('CellNumber', function ($http) {
+    return React.createClass({
+      getInitialState: function () {
+        return {isEditing: false};
+      },
+      onClick: function () {
+    	console.log('Cell Number console logged');
+        this.setState({isEditing: true}, function () {
+          this.refs.input.getDOMNode().focus();
+        });
+      },
+      onKeyPress: function (e) {
+        if (e.key === 'Enter') {
+          e.preventDefault();
+          this.handleUpdate();
+        }
+      },
+      handleUpdate: function () {
+        var value = this.refs.input.getDOMNode().value;
+        $http.post('/updateInstance', {
+          instance_id: this.props.instance._id,
+          attribute_name: this.props.attribute.name,
+          attribute_value: value
+        })
+          .success(function (data) {
+            console.log('Updated');
+          });
+        this.setState({isEditing: false});
+      },
+      render: function () {
+        if (this.state.isEditing) {
+          return (
+            <div className="input-group input-group-lg">
+              <input type="number" ref="input" className="form-control" defaultValue={this.props.value}
+              onBlur={this.handleUpdate} onKeyPress={this.onKeyPress} />
+            </div>
+            );
+        }
+        return (<div onClick={this.onClick}>{this.props.value}</div>);
+      }
+    });
+  });
