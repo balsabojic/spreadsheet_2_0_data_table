1109e659b3a058308c5781170e2d6aa3ba017951
Saroj Gautam <sarojnp2003@gmail.com>
2014-12-15 14:10:29 +0100

    adding date picker for date column

---
+++ app/assets/js/jsx/CellDate.jsx
@@ -0,0 +1,27 @@
+angular.module('Spreadsheet.jsx')
+  .factory('CellDate', function ($http) {
+    return React.createClass({
+      getInitialState: function () {
+    	  return {isEditing: false};
+      },
+      onChange: function (e) {
+    	  console.log('calling onChange function');
+    	  this.handleUpdate();
+      },
+      handleUpdate: function () {
+    	  var value = this.refs.input.getDOMNode().value;
+    	  $http.post('/updateInstance', {
+            instance_id: this.props.instance._id,
+            attribute_name: this.props.attribute.name,
+            attribute_value: value
+          })
+            .success(function (data) {
+              console.log('Updated');
+            });
+          this.setState({isEditing: false});
+      },
+      render: function () {
+    	return <input type="date" ref="input" className="form-control" defaultValue={this.props.value} onChange={this.onChange}></input>;
+      }
+    });
+  });
