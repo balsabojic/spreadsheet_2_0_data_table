c9f41445a959d93d5bd0cf4c78638a2c27a56c50
Duc Nguyen <duc.ngtuan@gmail.com>
2015-01-13 00:32:56 +0100

    popover for table header

--- app/assets/js/jsx/InstanceTableHeader.jsx
+++ app/assets/js/jsx/InstanceTableHeader.jsx
@@ -2,19 +2,34 @@ angular.module('Spreadsheet.jsx')
   .factory('InstanceTableHeader', function () {
     return React.createClass({
       displayName: 'InstanceTableHeader',
-      onClick: function(orderBy) {
+      onClick: function (orderBy) {
         this.props.onLinkClick(orderBy);
       },
       render: function () {
+        // define the component that will be used from ReactBootrap
+        var OverlayTrigger = ReactBootstrap.OverlayTrigger;
+        var Popover = ReactBootstrap.Popover;
+
         return (
           <thead>
             <tr>
             {this.props.headers.map(function (attribute) {
+              // the popover that will show info of the current attribute
+              var attrPopover = (
+                <Popover>
+                  <ul className="list-unstyled">
+                    <li><strong>Type:</strong> {attribute.type}</li>
+                    <li><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</li>
+                  </ul>
+                </Popover>
+                );
+              // return the header component for the current attribute
               return (
-                <th key={attribute.name} className={attribute.isFree ? "free-attr" : ""}>
-                  <a onClick={this.onClick.bind(this, attribute.name)}> {attribute.name} </a>
-                  <small className="label label-default">{attribute.type}</small>
-                </th>
+                <OverlayTrigger key={attribute.name} trigger="hover" placement="bottom" overlay={attrPopover}>
+                  <th className={attribute.isFree ? "free-attr" : ""}>
+                    <a onClick={this.onClick.bind(this, attribute.name)}> {attribute.name} </a>
+                  </th>
+                </OverlayTrigger>
                 );
             }.bind(this))}
             </tr>
