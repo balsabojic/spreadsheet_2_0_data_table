75db9647b304d59b541693b2633a6252eee2b67d
Saroj Gautam <sarojnp2003@gmail.com>
2015-01-19 15:00:42 +0100

    -Removed popover to display attribute type and moved them to MenuItem.

--- app/assets/js/jsx/InstanceTableHeader.jsx
+++ app/assets/js/jsx/InstanceTableHeader.jsx
@@ -18,33 +18,44 @@ angular.module('Spreadsheet.jsx')
         var ButtonToolbar = ReactBootstrap.ButtonToolbar;
         var DropdownButton = ReactBootstrap.DropdownButton;
         var MenuItem = ReactBootstrap.MenuItem;
+        var Well = ReactBootstrap.Well;
 
         return (
           <thead>
             <tr>
             {this.props.headers.map(function (attribute) {
               // the popover that will show info of the current attribute
-              var attrPopover = (
+              /*var attrPopover = (
                 <Popover>
                   <ul className="list-unstyled">
                     <li><strong>Type:</strong> {attribute.type}</li>
                     <li><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</li>
                   </ul>
                 </Popover>
-                );
+                );*/
               // return the header component for the current attribute
               return (
-                <OverlayTrigger key={attribute.name} trigger="hover" placement="bottom" overlay={attrPopover}>
+                <OverlayTrigger key={attribute.name}>
                   <th className={attribute.isFree ? "free-attr" : ""}>
                     <ButtonToolbar>
                       <DropdownButton bsSize="small" title={attribute.name}>
-                        <MenuItem onClick={this.onClick.bind(this, attribute.name, 1)} eventKey="1">Sort ascending</MenuItem>
+                      <Well bsSize="small">
+                      <ul className="list-unstyled">
+	                        <li><strong>Type:</strong> {attribute.type}</li>
+	                        <li><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</li>
+	                  </ul>
+	                  </Well>
+	                  <Well bsSize="small">
+                      <MenuItem onClick={this.onClick.bind(this, attribute.name, 1)} eventKey="1">Sort ascending</MenuItem>
                         <MenuItem onClick={this.onClick.bind(this, attribute.name, 0)} eventKey="2">Sort descending</MenuItem>
-                        <MenuItem divider />
-                        Filter
-                        <form>
-                          <input ref="filterInput" id={attribute.name} type="text" placeholder="Search..." onChange={this.handleFilterChange} />
-                        </form>
+                      </Well>
+                      	<MenuItem divider />
+                      	<Well bsSize="small">
+	                        Filter
+	                        <form>
+	                          <input ref="filterInput" id={attribute.name} type="text" placeholder="Search..." onChange={this.handleFilterChange} />
+	                        </form>
+                        </Well>
                       </DropdownButton>
                     </ButtonToolbar>
                   </th>
