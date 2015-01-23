fb9c9bee50b0195ad5a50e233a672dd9c9f51689
Saroj Gautam <sarojnp2003@gmail.com>
2015-01-23 20:41:26 +0100

    -- adding new button for adding new rows

--- app/assets/js/jsx/InstanceToolbar.jsx
+++ app/assets/js/jsx/InstanceToolbar.jsx
@@ -9,6 +9,30 @@ angular.module('Spreadsheet.jsx')
         alert(text);
       },
       
+      submit: function(){
+    	    var data_type = document.getElementById('dataId').value;
+	    	var header = document.getElementById('header').value;
+	    	
+	    	console.log("data_type " + data_type);
+	    	console.log("header " + header);
+	    	var instance = this.props.instance;
+	    	console.log("instance " + instance);
+	    	
+	    	if(header == '' || header =='undefined' ){
+	    		alert('Please fill header field'); 
+	    		return false;
+	    	}
+	    	
+	    	var data = {
+	                instance_id: instance._id,
+	                attribute_name: header,
+	        };
+	    	$http.post('', data)
+	        .success(function () {
+	        	PubSubService.publish('', [data]);
+	        });
+      },
+      
       render: function () {
         var Button = ReactBootstrap.Button;
         var ButtonToolbar = ReactBootstrap.ButtonToolbar;
@@ -18,7 +42,7 @@ angular.module('Spreadsheet.jsx')
 	    
 	    var PopIns = (
 	    		<Popover title="New Column">
-	    			<form>
+	    			<div>
 	    			    <Input type="select" ref="dataType" label='Data Type' id="dataId">
 	    			        <option value="String">String</option>
 	    			        <option value="Date">Date</option>
@@ -27,7 +51,7 @@ angular.module('Spreadsheet.jsx')
     			        </Input>
     			        <Input type="text" label='Header' required="required" id="header"/>
     			        <Input type="submit" bsStyle="primary" value="Add" onClick={this.submit} />
-	    			</form>
+	    			</div>
 	    		</Popover>	
 	    );
 	    
@@ -39,10 +63,13 @@ angular.module('Spreadsheet.jsx')
           		<Button className="spacebetween" bsStyle="primary" bsSize="small" onClick={this.redo.bind(this, "redo text")} >Redo</Button>
             </div>
             
+            <div className="alignright">
+            	<Button bsStyle="primary" bsSize="small">Add New Row</Button>
+            </div>
             
             <div className="alignright">
-            	<OverlayTrigger trigger="click" placement="bottom" overlay={PopIns}>
-            		<Button bsStyle="primary" bsSize="small">Add Column</Button>
+	            <OverlayTrigger trigger="click" placement="bottom" overlay={PopIns}>
+            		<Button bsStyle="primary" bsSize="small">Add New Column</Button>
             	</OverlayTrigger>
              </div>
 	      </div>
