7b3def5f0ee2a5514bf721a56c4cda489dc05619
Duc Nguyen <duc.ngtuan@gmail.com>
2015-02-09 16:25:47 +0100

    fix drop down menu table header style

--- app/assets/js/jsx/InstanceTableHeader.jsx
+++ app/assets/js/jsx/InstanceTableHeader.jsx
@@ -5,16 +5,13 @@ angular.module('Spreadsheet.jsx')
       onClick: function (orderBy, asc) {
         this.props.onLinkClick(orderBy, asc);
       },
-      filter: function(attribute_name) {
+      filter: function (attribute_name) {
         var filter_value = document.getElementsByName(attribute_name)[0].value;
         this.props.handleFilterChange(attribute_name, filter_value);
         document.getElementsByName(attribute_name)[0].value = '';
       },
       render: function () {
         // define the component that will be used from ReactBootrap
-        var OverlayTrigger = ReactBootstrap.OverlayTrigger;
-        var Popover = ReactBootstrap.Popover;
-
         var ButtonToolbar = ReactBootstrap.ButtonToolbar;
         var Button = ReactBootstrap.Button;
         var DropdownButton = ReactBootstrap.DropdownButton;
@@ -25,43 +22,27 @@ angular.module('Spreadsheet.jsx')
           <thead>
             <tr>
             {this.props.headers.map(function (attribute) {
-              // the popover that will show info of the current attribute
-              /*var attrPopover = (
-                <Popover>
-                  <ul className="list-unstyled">
-                    <li><strong>Type:</strong> {attribute.type}</li>
-                    <li><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</li>
-                  </ul>
-                </Popover>
-                );*/
               // return the header component for the current attribute
               return (
-                <OverlayTrigger key={attribute.name}>
-                  <th className={attribute.isFree ? "free-attr" : ""}>
-                    <ButtonToolbar>
-                      <DropdownButton bsSize="small" title={attribute.name}>
-                      <Well bsSize="small">
-                      <ul className="list-unstyled">
-	                    <li><strong>Type:</strong> {attribute.type}</li>
-	                    <li><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</li>
-	                  </ul>
-	                  </Well>
-	                  <Well bsSize="small">
+                <th className={attribute.isFree ? "free-attr" : ""}>
+                  <DropdownButton bsSize="small" title={attribute.name}>
+                    <MenuItem><strong>Type:</strong> {attribute.type}</MenuItem>
+                    <MenuItem><strong>Free attribute:</strong> {attribute.isFree ? "true" : "false"}</MenuItem>
+                    <MenuItem divider />
                       <MenuItem onClick={this.onClick.bind(this, attribute.name, 1)} eventKey="1">Sort ascending</MenuItem>
-                        <MenuItem onClick={this.onClick.bind(this, attribute.name, 0)} eventKey="2">Sort descending</MenuItem>
-                      	<MenuItem divider />
-                      	Filter
-	                      <form>
-	                        <input name={attribute.name} id="filter_input" type="text" placeholder="Search..."  />
-                            <ButtonToolbar>
-                              <Button bsStyle="primary" bsSize="xsmall" onClick={this.filter.bind(this, attribute.name)}>Find</Button>
-                            </ButtonToolbar>
-	                      </form>
-                        </Well>
-                      </DropdownButton>
-                    </ButtonToolbar>
-                  </th>
-                </OverlayTrigger>
+                      <MenuItem onClick={this.onClick.bind(this, attribute.name, 0)} eventKey="2">Sort descending</MenuItem>
+                      <MenuItem divider />
+                    <MenuItem>
+                    Filter
+                      <form>
+                        <input name={attribute.name} id="filter_input" type="text" placeholder="Search..."  />
+                        <ButtonToolbar>
+                          <Button bsStyle="primary" bsSize="xsmall" onClick={this.filter.bind(this, attribute.name)}>Find</Button>
+                        </ButtonToolbar>
+                      </form>
+                    </MenuItem>
+                  </DropdownButton>
+                </th>
                 );
             }.bind(this))}
             </tr>
--- app/assets/js/jsx/InstanceToolbar.jsx
+++ app/assets/js/jsx/InstanceToolbar.jsx
@@ -5,24 +5,24 @@ angular.module('Spreadsheet.jsx')
       pubsubHandle: {},
       undo_list: [],
       undo_list_pointer: 0,
-      
+
       getInitialState: function () {
-          return {
-        	  isString: false,
-        	  isNumber: false,
-        	  isBoolean: false,
-        	  isDate: false,
-        	  isReference: false
-          };
-        },
-        
+        return {
+          isString: false,
+          isNumber: false,
+          isBoolean: false,
+          isDate: false,
+          isReference: false
+        };
+      },
+
       componentDidMount: function () {
         this.pubsubHandle['cellUpdate'] = PubSubService.subscribe('cellUpdate', this.onCellUpdate);
       },
 
       componentWillUnmount: function () {
         _.forIn(this.pubsubHandle, function (handle) {
-        PubSubService.unsubscribe(handle);
+          PubSubService.unsubscribe(handle);
         });
       },
 
@@ -32,7 +32,7 @@ angular.module('Spreadsheet.jsx')
         this.undo_list_pointer = this.undo_list.length;
       },
 
-      undo: function(text) {
+      undo: function (text) {
         if (this.undo_list_pointer > 0 && this.undo_list.length > 0) {
           this.undo_list_pointer--;
           var e = this.undo_list[this.undo_list_pointer];
@@ -47,9 +47,10 @@ angular.module('Spreadsheet.jsx')
         }
       },
 
-      redo: function(text) {
+      redo: function (text) {
         if (this.undo_list.length > 0 && this.undo_list_pointer <= (this.undo_list.length - 1)) {
-          var e = this.undo_list[this.undo_list_pointer];;
+          var e = this.undo_list[this.undo_list_pointer];
+          ;
           var temp = e['attribute_value'];
           e['attribute_value'] = e['attribute_value_old'];
           e['attribute_value_old'] = temp;
@@ -61,180 +62,178 @@ angular.module('Spreadsheet.jsx')
           );
         }
       },
-      
-      submitNewRow: function(){
-    	    var header = document.getElementById("headerType").selectedOptions[0].label;
-    	    var value = document.getElementById('inputVal').value;
-	    	var type_id = this.props.type_Id;
-	    	
-	    	if(value === ''){
-	    		alert('Please fill Value field'); 
-	    		return false;
-	    	}
-	    	
-	    	var data = {
-	    			type_id: type_id,
-	    			attribute_name: header,
-	                attribute_value	: value
-	        };
-	    	$http.post('/addInstance', data)
-	        .success(function () {
-	        	PubSubService.publish('', [data]);
-	        });
+
+      submitNewRow: function () {
+        var header = document.getElementById("headerType").selectedOptions[0].label;
+        var value = document.getElementById('inputVal').value;
+        var type_id = this.props.type_Id;
+
+        if (value === '') {
+          alert('Please fill Value field');
+          return false;
+        }
+
+        var data = {
+          type_id: type_id,
+          attribute_name: header,
+          attribute_value: value
+        };
+        $http.post('/addInstance', data)
+          .success(function () {
+            PubSubService.publish('', [data]);
+          });
       },
-      
-      submitNewColumn: function(){
-    	    var data_type = document.getElementById('dataId').value;
-	    	var header = document.getElementById('header').value;
-	    	
-	    	var type_id = this.props.type_Id;
-	    	
-	    	if(header == ''){
-	    		alert('Please fill header field'); 
-	    		return false;
-	    	}
-	    	
-	    	var data = {
-	    			type_id: type_id,
-	    			attribute_name: header,
-	                attribute_type	: data_type
-	        };
-	    	$http.post('/addTypeAttribute', data)
-	        .success(function () {
-	        	PubSubService.publish('onNewColumn', [data]);
-	        });
+
+      submitNewColumn: function () {
+        var data_type = document.getElementById('dataId').value;
+        var header = document.getElementById('header').value;
+
+        var type_id = this.props.type_Id;
+
+        if (header == '') {
+          alert('Please fill header field');
+          return false;
+        }
+
+        var data = {
+          type_id: type_id,
+          attribute_name: header,
+          attribute_type: data_type
+        };
+        $http.post('/addTypeAttribute', data)
+          .success(function () {
+            PubSubService.publish('onNewColumn', [data]);
+          });
       },
-      
-      checkForNumber : function(evt){
-    	  var charCode = (evt.which) ? evt.which : event.keyCode
-    	  if (charCode > 31 && (charCode < 48 || charCode > 57))
-    	        evt.preventDefault();
+
+      checkForNumber: function (evt) {
+        var charCode = (evt.which) ? evt.which : event.keyCode
+        if (charCode > 31 && (charCode < 48 || charCode > 57))
+          evt.preventDefault();
       },
-      
-      change: function (){
-    	  var dataType = document.getElementById('headerType').value;
-    	  
-    	  if(dataType === "string"){
-    		  this.setState({isString: true});
-    	  }
-    	  
-    	  else if(dataType === "date"){
-    		  this.setState({isDate: true});
-    	  }
-    	  
-    	  else if(dataType === "boolean"){
-    		  this.setState({isBoolean: true});
-    	  }
-    	  
-    	  else if(dataType === "number"){
-    		  this.setState({isNumber: true});
-    	  }
-    	  
-    	  else if(dataType === 'reference'){
-    		  this.setState({isReference: true});
-    	  }
+
+      change: function () {
+        var dataType = document.getElementById('headerType').value;
+
+        if (dataType === "string") {
+          this.setState({isString: true});
+        }
+
+        else if (dataType === "date") {
+          this.setState({isDate: true});
+        }
+
+        else if (dataType === "boolean") {
+          this.setState({isBoolean: true});
+        }
+
+        else if (dataType === "number") {
+          this.setState({isNumber: true});
+        }
+
+        else if (dataType === 'reference') {
+          this.setState({isReference: true});
+        }
       },
-      
+
       render: function () {
         var Button = ReactBootstrap.Button;
         var ButtonToolbar = ReactBootstrap.ButtonToolbar;
         var Popover = ReactBootstrap.Popover;
-	    var OverlayTrigger = ReactBootstrap.OverlayTrigger
-	    var Input = ReactBootstrap.Input
-	    
-	    var PopIns = (
-	    		<Popover title="Add New Column">
-	    			<form>
-	    			    <Input type="select" ref="dataType" label='Data Type' id="dataId">
-	    			        <option value="string">String</option>
-	    			        <option value="date">Date</option>
-	    			        <option value="boolean">Boolean</option>
-	    			        <option value="number">Number</option>
-    			        </Input>
-    			        <Input type="text" label='Header' required="required" id="header"/>
-    			        <Input type="submit" bsStyle="primary" value="Add" onClick={this.submitNewColumn} />
-	    			</form>
-	    		</Popover>	
-	    );
-	    
-	    var inputValue = <Input type="text" label='Value' required id="inputVal"/>;
-  	  
-  	    if(this.state.isString){
-  		  inputValue = <Input type="text" label='Value' required id="inputVal"/>
-  		  this.state.isString = false;
-  		  
-  	    }
-  	    else if(this.state.isDate){
-  		  inputValue = <Input type="date" label='Value' required id="inputVal"/>;
-	          this.state.isDate = false;
-	          
-	    }
-  	    else if(this.state.isBoolean){
-  		  inputValue = <Input type="select" ref="dataType" label='Value' id="inputVal" onChange={this.change}>
-  			        	<option value="true">True</option>
-  			        	<option value="false">False</option>
-  			        	<option value="undefined">Undefined</option>
-  			        </Input>
-  		  this.state.isBoolean = false;
-	    }
-  	    else if(this.state.isNumber){
-  		  inputValue =  <Input type="number" label='Value' required id="inputVal" onKeyPress={this.checkForNumber}/>
-  		  this.state.isNumber = false;
-	    }
-  	    
-  	    else if(this.state.isReference){
-  	    	/*inputValue = <Input type="select" ref="dataType" label='Value' id="inputVal" onChange={this.change}>
-	        	<option value="true">True</option>
-	        	<option value="false">False</option>
-	        	<option value="undefined">Undefined</option>
-	        </Input>
-	        this.state.isReference = false;*/
-  	    	
-  	    	inputValue =  <Input type="string" label='Value' id="inputVal" value='' disabled/>
-  	    	this.state.isReference = false;
-  	    }
-  	  
-  	    var rowPopIns = (
-	    		<Popover title="Add New Row">
-	    			<form>
-		    	      <Input type="select" label='Header' id="headerType" onChange={this.change}>  
+        var OverlayTrigger = ReactBootstrap.OverlayTrigger
+        var Input = ReactBootstrap.Input
+
+        var PopIns = (
+          <Popover title="Add New Column">
+            <form>
+              <Input type="select" ref="dataType" label='Data Type' id="dataId">
+                <option value="string">String</option>
+                <option value="date">Date</option>
+                <option value="boolean">Boolean</option>
+                <option value="number">Number</option>
+              </Input>
+              <Input type="text" label='Header' required="required" id="header"/>
+              <Input type="submit" bsStyle="primary" value="Add" onClick={this.submitNewColumn} />
+            </form>
+          </Popover>
+          );
+
+        var inputValue = <Input type="text" label='Value' required id="inputVal"/>;
+
+        if (this.state.isString) {
+          inputValue = <Input type="text" label='Value' required id="inputVal"/>
+          this.state.isString = false;
+
+        }
+        else if (this.state.isDate) {
+          inputValue = <Input type="date" label='Value' required id="inputVal"/>;
+          this.state.isDate = false;
+
+        }
+        else if (this.state.isBoolean) {
+          inputValue = <Input type="select" ref="dataType" label='Value' id="inputVal" onChange={this.change}>
+            <option value="true">True</option>
+            <option value="false">False</option>
+            <option value="undefined">Undefined</option>
+          </Input>
+          this.state.isBoolean = false;
+        }
+        else if (this.state.isNumber) {
+          inputValue = <Input type="number" label='Value' required id="inputVal" onKeyPress={this.checkForNumber}/>
+          this.state.isNumber = false;
+        }
+
+        else if (this.state.isReference) {
+          /*inputValue = <Input type="select" ref="dataType" label='Value' id="inputVal" onChange={this.change}>
+           <option value="true">True</option>
+           <option value="false">False</option>
+           <option value="undefined">Undefined</option>
+           </Input>
+           this.state.isReference = false;*/
+
+          inputValue = <Input type="string" label='Value' id="inputVal" value='' disabled/>
+          this.state.isReference = false;
+        }
+
+        var rowPopIns = (
+          <Popover title="Add New Row">
+            <form>
+              <Input type="select" label='Header' id="headerType" onChange={this.change}>
 	  			      {this.props.headers.map(function (attribute) {
-	  		              return (
-	  		            		<option value={attribute.type} label={attribute.name}> {attribute.name} </option>
-	  		            	);
-	  		            }.bind(this))}
-	  			        
-	  			        </Input>
+                  return (
+                    <option value={attribute.type} label={attribute.name}> {attribute.name} </option>
+                    );
+                }.bind(this))}
+
+              </Input>
 	  			        {inputValue}
-	  			        <Input type="submit" bsStyle="primary" value="Add" onClick={this.submitNewRow} />
-	    			</form>
-	    		</Popover>	
-	    );
-	    
+              <Input type="submit" bsStyle="primary" value="Add" onClick={this.submitNewRow} />
+            </form>
+          </Popover>
+          );
+
         return (
           <ButtonToolbar>
-          <div className="marginbelow">
-          	<div className="alignleft">
-          		<Button bsStyle="primary" bsSize="small" onClick={this.undo.bind(this,"undo text")} >Undo</Button>
-          		<Button className="spacebetween" bsStyle="primary" bsSize="small" onClick={this.redo.bind(this, "redo text")} >Redo</Button>
+            <div className="marginbelow">
+              <div className="alignleft">
+                <Button bsStyle="primary" bsSize="small" onClick={this.undo.bind(this, "undo text")} >Undo</Button>
+                <Button className="spacebetween" bsStyle="primary" bsSize="small" onClick={this.redo.bind(this, "redo text")} >Redo</Button>
+              </div>
+
+              <div className="alignright">
+                <OverlayTrigger trigger="click" placement="bottom" overlay={rowPopIns}>
+                  <Button bsStyle="primary" bsSize="small">Add New Row</Button>
+                </OverlayTrigger>
+              </div>
+
+              <div className="alignright">
+                <OverlayTrigger trigger="click" placement="bottom" overlay={PopIns}>
+                  <Button bsStyle="primary" bsSize="small">Add New Column</Button>
+                </OverlayTrigger>
+              </div>
             </div>
-            
-            <div className="alignright">
-            	<OverlayTrigger trigger="click" placement="bottom" overlay={rowPopIns}>
-            		<Button bsStyle="primary" bsSize="small">Add New Row</Button>
-            	</OverlayTrigger>
-            </div>
-            
-            <div className="alignright">
-	            <OverlayTrigger trigger="click" placement="bottom" overlay={PopIns}>
-            		<Button bsStyle="primary" bsSize="small">Add New Column</Button>
-            	</OverlayTrigger>
-             </div>
-	      </div>
           </ButtonToolbar>
-          
-          
-        );
+          );
       }
     });
   });
